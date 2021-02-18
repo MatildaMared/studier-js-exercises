923d8b65006caac9b5774e4afb5d450bff18b516
MatildaMared <matildamared@live.se>
2021-02-18 14:05:42 +0100

    complete numbers exercise

--- numbers/app.js
+++ numbers/app.js
@@ -12,12 +12,22 @@ const isNotEqual = () => {
 /* --- And here --- */
 
 // check if number is less than 31, if it is call function isEqual. Otherwise call function isNotEqual
-
+if (number < 31) {
+  isEqual();
+} else {
+  isNotEqual();
+}
 
 // check if number is NOT equal to 31, if it is not equal call function isNotEqual, otherwise call function isEqual
-
+if (number != 31) {
+  isNotEqual();
+} else {
+  isEqual();
+}
 
 // check if number is STRICTLY equal to 31, if it is call function isEqual. Otherwise call function isNotEqual
-
-
-
+if (number === 31) {
+  isEqual();
+} else {
+  isNotEqual();
+}
