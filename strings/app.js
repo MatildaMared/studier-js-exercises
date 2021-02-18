9c6f2d94a2a1f053488c1f20c1893f3c086fac2a
MatildaMared <matildamared@live.se>
2021-02-18 13:48:39 +0100

    complete string exercise

--- strings/app.js
+++ strings/app.js
@@ -1,6 +1,5 @@
-
-
 // Make a variable with your name
-
+let name = "Matilda";
 
 // write to h1, "I, NAME is a great programmer!"
+document.querySelector(".heading").innerText = `I, ${name}, am a great programmer!`;
