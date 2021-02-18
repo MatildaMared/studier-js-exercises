ec57d8d8432c614011c9f1e18b0d7ce0a0012abd
MatildaMared <matildamared@live.se>
2021-02-18 14:14:52 +0100

    complete arrays exercise

--- arrays/app.js
+++ arrays/app.js
@@ -6,10 +6,14 @@ const shoppingList = ["Tomatoes", "Eggs", "Potatoes", "Ham", "Milk"];
 /* --- And here --- */
 
 // Loop through the array to print each item on new line
-
+for (let i = 0; i < shoppingList.length; i++) {
+    console.log(shoppingList[i]);
+}
 
 // if the number of items is more than 3 alert('Man, how can I carry so much?');
-
+if (shoppingList.length > 3) {
+    alert("Man, how can I carry so much?");
+}
 
 // Remove the last item, Milk, and console.log it to the console
-
+console.log(shoppingList.pop());
