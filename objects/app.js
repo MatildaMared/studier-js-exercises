91cbcfa1f5a1c98a0e1b9a598b60512e16bae5d4
MatildaMared <matildamared@live.se>
2021-02-18 14:01:38 +0100

    complete object exercise

--- objects/app.js
+++ objects/app.js
@@ -11,3 +11,9 @@ const teacher = {
 /* --- And here --- */
 
 // Print out to document, on a new line, the key / value pairs of the object.
+
+for (let key in teacher) {
+    let pair = document.createElement("h2");
+    pair.innerText = `${key}: ${teacher[key]}`;
+    document.body.appendChild(pair);
+}
